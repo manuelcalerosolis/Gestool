7af34ff78c614b09d42d3c5bdbe10bee19df4f94
Manuel Calero Solís <mcalero@gestool.es>
2012-12-30 00:42:14 +0100

    Primero; Primero

---
+++ mak/Hr.bat
@@ -0,0 +1,29 @@
+rem RC file to 32 bits resources DLL
+rem syntax: rc2dll32.bat Your_rc_file  !!! without the .RC extension
+
+del \fw195\camero\obj805\factu.obj
+
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage2.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstDialog.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstPda.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\MenuPda.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstTactil.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\RptApolo.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage1.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage3.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage4.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage5.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage6.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage7.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage8.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage9.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage10.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage11.Rc
+\vce\bin\rc /r /d__FLAT__ /d__MK__ /i\bcc55\include ..\Resource\GstImage12.Rc
+
+del *.map
+del *.tds
+
+echo done!
+
+call hx.bat
---
+++ mak/Hx.bat
@@ -0,0 +1,6 @@
+cd \fw195\gestool\
+
+\BCC55\BIN\MAKE -S -fHX.MAK -D__GST__
+
+GstApolo.exe /TERMINAL
+
